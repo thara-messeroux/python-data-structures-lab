f82f613f631f3dee4d45f31b0bd29608d86ca776
thara-messeroux <tharamesseroux@gmail.com>
2026-05-25 19:14:35 -0400

    Complete Exercise 6: List comprehension to celebrate students

--- exercises.py
+++ exercises.py
@@ -103,3 +103,17 @@ def list_home_town_items():
 
 # Call the function and print the result
 print('Exercise 5:', list_home_town_items())
+
+# Exercise 6: Celebrate Students
+#
+# Using the list of students and a list comprehension, assign to a variable named awesome_students a new list containing strings.
+# For example: ["Tina is awesome!", "Fred is awesome!", "Wilma is awesome!"]
+
+def create_awesome_students():
+    # your code here
+    students = ['Alice', 'Bob', 'Charlie']
+    awesome_students = [f"{student} is awesome!" for student in students]
+    return awesome_students 
+
+# Call the function and print the result
+print('Exercise 6:', create_awesome_students())
