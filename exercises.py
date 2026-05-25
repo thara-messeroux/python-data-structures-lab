31b1a1dc50d2fc0fdc461c16e67d256abb5cb242
thara-messeroux <tharamesseroux@gmail.com>
2026-05-25 19:15:24 -0400

    Complete Exercise 7: Filter foods using list comprehension with conditional

--- exercises.py
+++ exercises.py
@@ -117,3 +117,18 @@ def create_awesome_students():
 
 # Call the function and print the result
 print('Exercise 6:', create_awesome_students())
+
+
+# Exercise 7: Filter Foods
+#
+# Assign to a variable named foods_with_an_a the result of list comprehension that filters the foods tuple to only include food strings that contain the letter 'a'.
+# For example, if foods is a tuple of ('Taco', 'Burrito', 'Sandwich'), foods_with_an_a would be a list equal to ['Taco', 'Sandwich']
+
+def filter_foods_with_a():
+    # your code here
+    foods = ('Pizza', 'Burger', 'Salad')
+    foods_with_an_a = [food for food in foods if 'a' in food.lower()]
+    return foods_with_an_a
+
+# Call the function and print the result
+print('Exercise 7:', filter_foods_with_a())
